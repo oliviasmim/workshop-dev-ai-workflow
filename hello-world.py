231810da8d2224024c639b25840a13c5d1d5987c
Iasmim Oliveira <iasmim.oliveira@ifood.com.br>
2025-06-04 19:43:20 -0300

    Add hello-world.py with basic function

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+def hello_world():
+    """
+    A simple function that prints a greeting message.
+    
+    Returns:
+        None
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    hello_world()
